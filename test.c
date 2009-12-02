e5e9c5cc80592c5c9dbd4be4718fac1f4df6aadd
Makoto Yamashita <makotoy@host-12-54.dhcp.math.jussieu.fr>
2009-12-02 11:38:43 +0100

    test change

--- test.c
+++ test.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-long double log2l(long double temp){ 
-  long double answer; 
-  answer=log10l(temp)/log10l(2.0); 
-  return(answer); 
-} 
-
 int main (int argc, const char * argv[]) {
-//  printf("answer: %Lf\n", log2l(3.0));
 	char c[2] = {1};
 	c[0] = 48;
 	c[1] = '\0';
